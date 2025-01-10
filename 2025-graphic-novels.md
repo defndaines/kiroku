db659ad113cbeb84c2232d9686829b1531844482
Michael Daines <953037+defndaines@users.noreply.github.com>
2025-01-10 13:12:39 -0800

    We Sing From the Heart (3.5)

--- 2025-graphic-novels.md
+++ 2025-graphic-novels.md
@@ -3,8 +3,8 @@
 Separating out graphic novels, comics, and manga from other books read.
 
 Tags to include information such as genre. The most recently finished book is
-on top, and the first book finished in a month is tagged with the month in
-**bold**.
+on top.
 
 | Title | Author | Year | Country | Rating | Format | Pages | Tags |
 | --- | --- | :---: | --- | :---: | --- | ---: | --- |
+| We Sing From the Heart: How The Slants® Took Their Fight for Free Speech to the Supreme Court | Mia Wenjen | 2024 | U.S. | 3.5 | ebook | 37 | nonfiction, history, music, biography |
--- data/analysis.sh
+++ data/analysis.sh
@@ -1,92 +1,94 @@
-# 2024 Reading
+# 2025 Reading
 
 ## Total Book Count
 
-tail -n +9 2024-reading.md | wc -l
+tail -n +8 2025-reading.md | wc -l
 
-tail -n +9 2024-reading.md | grep -vc "graphic novel"
-
-tail -n +9 2024-reading.md | grep -c "graphic novel"
+tail -n +10 2025-graphic-novels.md | wc -l
 
 ## Authors
 
-tail -n +9 2024-reading.md | cut -d \| -f 3 | sed 's/, /\n/g' | sed 's/^ *//' | sed 's/ *$//' | sort | uniq -c | sort -n
+tail -n +8 2025-reading.md | cut -d \| -f 3 | sed 's/, /\n/g' | sed 's/^ *//' | sed 's/ *$//' | sort | uniq -c | sort -n
 
-tail -n +9 2024-reading.md | grep -v "graphic novel" | cut -d \| -f 3 | sed 's/, /\n/g' | sed 's/^ *//' | sed 's/ *$//' | sort | uniq -c | sort -n | grep -v " 1 "
+tail -n +8 2025-reading.md | cut -d \| -f 3 | sed 's/, /\n/g' | sed 's/^ *//' | sed 's/ *$//' | sort | uniq -c | sort -n | grep -v " 1 "
 
 ## Publication Year
 
 ### Books older than 10 year
 
-tail -n +9 2024-reading.md | cut -d \| -f 4 | sort | sed -n '1,/2014/p' | wc -l
+# tail -n +8 2025-reading.md | cut -d \| -f 4 | sort | sed -n '1,/2015/p' | wc -l
 
 ### Books from last year (roughly)
 
-grep -c '| 202[34]' 2024-reading.md
+grep -c '| 202[45]' 2025-reading.md
 
 ## Countries
 
-tail -n +9 2024-reading.md | cut -d \| -f 5 | sed 's/, /\n/g' | sed 's/^ *//' | sed 's/ *$//' | sort | uniq -c | sort -n
+tail -n +8 2025-reading.md | cut -d \| -f 5 | sed 's/, /\n/g' | sed 's/^ *//' | sed 's/ *$//' | sort | uniq -c | sort -n
 
-tail -n +9 2024-reading.md | cut -d \| -f 5 | sed 's/, /\n/g' | sed 's/^ *//' | sed 's/ *$//' | sort | uniq -c | sort -nr | head
+tail -n +8 2025-reading.md | cut -d \| -f 5 | sed 's/, /\n/g' | sed 's/^ *//' | sed 's/ *$//' | sort | uniq -c | sort -nr | head
 
 ### Non-U.S. or U.K. works
 
-tail -n +9 2024-reading.md | cut -d \| -f 5 | sed 's/, /\n/g' | sed 's/^ *//' | sed 's/ *$//' | grep -v "U\." | sort -u
+tail -n +8 2025-reading.md | cut -d \| -f 5 | sed 's/, /\n/g' | sed 's/^ *//' | sed 's/ *$//' | grep -v "U\." | sort -u
 
 ### Count of non-U.[SK]. work
 
-tail -n +9 2024-reading.md | cut -d \| -f 5 | sed 's/, /\n/g' | grep -vc "U\."
+tail -n +8 2025-reading.md | grep -vc "U\.[SK]\."
 
 ## Genres
 
 ### Fiction count
 
-tail -n +9 2024-reading.md | grep -vc "nonfiction"
+tail -n +8 2025-reading.md | grep -vc "nonfiction"
 
-tail -n +9 2024-reading.md | grep -v "graphic novel" | grep -vc "nonfiction"
+tail -n +10 2025-graphic-novels.md | grep -vc "nonfiction"
 
 ### Nonfiction count
 
-tail -n +9 2024-reading.md | grep -c "nonfiction"
+tail -n +8 2025-reading.md | grep -c "nonfiction"
 
-tail -n +9 2024-reading.md | grep -v "graphic novel" | grep -c "nonfiction"
+tail -n +10 2025-graphic-novels.md | grep -c "nonfiction"
 
 ### Genre full list
 
-tail -n +9 2024-reading.md | cut -d \| -f 9 | sed 's/,/\n/g' | awk '{$1=$1};1' | sort | uniq -c
+tail -n +8 2025-reading.md | cut -d \| -f 9 | sed 's/,/\n/g' | awk '{$1=$1};1' | sort | uniq -c
 
-tail -n +9 2024-reading.md | grep -v "graphic novel"
+tail -n +10 2025-graphic-novels.md | cut -d \| -f 9 | sed 's/,/\n/g' | awk '{$1=$1};1' | sort | uniq -c
 
 ### Most common genres
 
-tail -n +9 2024-reading.md | cut -d \| -f 9 | sed 's/,/\n/g' | awk '{$1=$1};1' | sort | uniq -c | grep -v "graphic novel" | grep -v manga | grep -v reread | grep -v novella | sort -nr | head
+tail -n +8 2025-reading.md | cut -d \| -f 9 | sed 's/,/\n/g' | awk '{$1=$1};1' | sort | uniq -c | grep -v reread | grep -v novella | sort -nr | head
 
 ## Formats
 
-tail -n +9 2024-reading.md | cut -d \| -f 7 | sort | uniq -c
+tail -n +8 2025-reading.md | cut -d \| -f 7 | sort | uniq -c
 
-tail -n +9 2024-reading.md | grep -v "graphic novel" | cut -d \| -f 7 | sort | uniq -c
-
-tail -n +9 2024-reading.md | grep "graphic novel" | cut -d \| -f 7 | sort | uniq -c
+tail -n +10 2025-graphic-novels.md | cut -d \| -f 7 | sort | uniq -c
 
 ## Ratings
 
-tail -n +9 2024-reading.md | cut -d \| -f 6 | sort -n | uniq -c
+tail -n +8 2025-reading.md | cut -d \| -f 6 | sort -n | uniq -c
 
 ### 5.0 Books
 
-tail -n +9 2024-reading.md | grep " 5.0 " | cut -d \| -f 2-3
+grep " 5.0 " 2025-reading.md | cut -d \| -f 2-3
 
 ## Page Count
 
-tail -n +9 2024-reading.md | cut -d \| -f 8 | sort -n
+tail -n +8 2025-reading.md | cut -d \| -f 8 | sort -n
 
-tail -n +9 2024-reading.md | grep -v "graphic novel" | cut -d \| -f 8 | sort -n
+tail -n +8 2025-reading.md | cut -d \| -f 8 | awk '{s+=$1}END{print "average:",s/NR}' RS="\n"
 
-## Sort in context
+tail -n +10 2025-graphic-novels.md | cut -d \| -f 8 | sort -n
 
-tail -n +9 2024-reading.md | sort -t\| -k 6 -n
+tail -n +10 2025-graphic-novels.md | cut -d \| -f 8 | awk '{s+=$1}END{print "average:",s/NR}' RS="\n"
+
+## Sort in context (e.g., rating [6] or pages [8])
+
+tail -n +8 2025-reading.md | sort -t\| -k 6 -n
+
+tail -n +8 2025-reading.md | sort -t\| -k 8 -n
 
 # Awards
 
@@ -102,27 +104,27 @@ sed -n '1,/Best Novella/p' nebula-award.md | grep -c '\[x\]'
 
 sed -n '1,/Ray Bradbury Award/p' nebula-award.md | grep -c '\[x\]'
 
-## Other awards
+## Other awards (for loop audits)
+
+grep "^Count:" [a-z]*.md
 
 for award in booker-prize.md carnegie-medal.md dublin-award.md golden-poppy.md great-american-novels.md kirkus.md locus-award.md national-book-award.md nobel-literature.md pulitzer.md womens.md world-fantasy.md; do
   echo $award
   grep -c '\[x\]' $award
 done
 
-grep "^Count:" [a-z]*.md
+## Yearly (since 2024) Analysis
 
-## Yearly (since 2023) Analysis
-
-git diff dc21ff7 countries-read.md
+git diff c10334f countries-read.md
 
 for f in [a-z]*.md; do
-  head -1 $f
-  git diff dc21ff7 $f | grep "^[-+]Count:"
+  head -1 "$f"
+  git diff c10334f "$f" | grep "^[-+]Count:"
 done
 
 ## TBR Documents
 
-cut -d \| -f 5 data/audiobooks.txt | sed 's/,/\n/g' | awk '{$1=$1};1' | sort | uniq -c | grep -v "graphic novel" | grep -v manga | grep -v reread | grep -v novella | sort -n | tail
+cut -d \| -f 5 data/audiobooks.txt | sed 's/,/\n/g' | awk '{$1=$1};1' | sort | uniq -c | grep -v reread | grep -v novella | sort -n | tail
 
 ### 10 short books
 
